1ade3ab84f6737d4ab7e988ce81c4371919fb87f
AceAyon <99069976+AceAyon@users.noreply.github.com>
2022-02-05 14:24:58 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import logo
 import random
-print(logo.welcome_logo)
-h = logo.heads
-t = logo.tails
-HeadOrTails = ["h", "t"]
-# guessing
-print("Coin Flipped.....\n")
-flip = random.choice(HeadOrTails)
-guess = input("\nGuess =>\ntype 'h' for heads or 't' for tails: \n")
-    
-if guess == flip:
-    print("\nYou're correct.....")
-    if flip == "h":
-        print("\nIt was.... ", h)
+
+def game():
+    print(logo.welcome_logo)
+    h = logo.heads
+    t = logo.tails
+    HeadOrTails = ["h", "t"]
+    # guessing
+    print("Coin Flipped.....\n")
+    flip = random.choice(HeadOrTails)
+    guess = input("\nGuess =>\ntype 'h' for heads or 't' for tails: \n")
+        
+    if guess == flip:
+        print("\nYou're correct.....")
+        if flip == "h":
+            print("\nIt was.... ", h)
+        else:
+            print("\nIt was.... ", t)
     else:
-        print("\nIt was.... ", t)
-else:
-    print("\nYou're wrong")
-    if flip == "h":
-        print("\nIt was.... ", h)
-    else:
-        print("\nIt was.... ", t)
+        print("\nYou're wrong")
+        if flip == "h":
+            print("\nIt was.... ", h)
+        else:
+            print("\nIt was.... ", t)
+            
+    yn = input("Restart? 'y' or 'n': ")
+    if yn == "y":
+        game()
+    else: print("Goodbye")
+game()
