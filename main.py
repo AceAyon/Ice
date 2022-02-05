6ccc5c4886968e276cdb36f233e2054b7a188020
AceAyon <99069976+AceAyon@users.noreply.github.com>
2022-02-05 14:11:09 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import logo
+import random
+print(logo.welcome_logo)
+h = logo.heads
+t = logo.tails
+HeadOrTails = ["h", "t"]
+# guessing
+print("Coin Flipped.....\n")
+flip = random.choice(HeadOrTails)
+guess = input("\nGuess =>\ntype 'h' for heads or 't' for tails: \n")
+    
+if guess == flip:
+    print("\nYou're correct.....")
+    if flip == "h":
+        print("\nIt was.... ", h)
+    else:
+        print("\nIt was.... ", t)
+else:
+    print("\nYou're wrong")
+    if flip == "h":
+        print("\nIt was.... ", h)
+    else:
+        print("\nIt was.... ", t)
